bfe7aa0ded3d84df8179e1dc42c05c0bec124a7c
Vyacheslav Pritykin <pritykin.amir@gmail.com>
2016-04-23 23:08:38 +0300

    Added ability to build nuget package. See https://www.nuget.org/packages/Sitecore.CacheObserver

--- Sitecore.CacheObserver/Properties/AssemblyInfo.cs
+++ Sitecore.CacheObserver/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.CacheObserver")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sitecore CacheObserver")]
+[assembly: AssemblyDescription("Advanced cache monitoring tool for Sitecore that was designed to replace built-in /sitecore/admin/cache.aspx")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sitecore.CacheObserver")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Vyacheslav Pritykin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
